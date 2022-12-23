3aa53e8074259bb05effe85d139e1052d336236b
lebalans21-60TH2 <46484885+lebalans21-60TH2@users.noreply.github.com>
2022-12-23 21:08:17 +0700

    push

---
+++ client/build/precache-manifest.f14b22f7950a85080d414b4f545ca1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcc0fdfde1f2e86e6f28",
+    "url": "/static/css/main.79f920a1.chunk.css"
+  },
+  {
+    "revision": "dcc0fdfde1f2e86e6f28",
+    "url": "/static/js/main.dcc0fdfd.chunk.js"
+  },
+  {
+    "revision": "c15ab27aea3345ef45ad",
+    "url": "/static/css/1.029290d9.chunk.css"
+  },
+  {
+    "revision": "c15ab27aea3345ef45ad",
+    "url": "/static/js/1.c15ab27a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f17aeb81d5928dd74d005ca4885b030",
+    "url": "/index.html"
+  }
+];
