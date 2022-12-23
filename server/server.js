e56f621dd0e4ca6d9efe080cd4a47066325bc9f5
lebalans21-60TH2 <46484885+lebalans21-60TH2@users.noreply.github.com>
2022-12-23 21:15:45 +0700

    push

--- server/server.js
+++ server/server.js
@@ -1024,6 +1024,11 @@ app.get("/api/client/download/:id", auth, admin, (req, res) => {
 });
 
 // DEFAULT
+app.use(express.static(path.join(__dirname,"../client/build")));
+app.get("*",(req,res) =>{
+    res.sendFile(path.resolve(__dirname,"../client/build/index.html"));
+})
+
 if (process.env.NODE_ENV === "production") {
     const path = require("path");
     app.get("/*", (req, res) => {
